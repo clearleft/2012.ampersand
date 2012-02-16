68152f97f189d1fba5ef33fc65ad12e31c8ce0c4
Bevan Stephens <bevan@clearleft.com>
2012-02-16 10:27:48 +0000

    Added first version of full site

---
+++ eric-spiekerman.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8" />
+<title>Ampersand 2012 &middot; The Web Typography Conference</title>
+<meta name="description" content="Ampersand is an affordable one-day event for knowledgable web designers and typography enthusiasts, held in Brighton on 15 June 2012." />
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+<link rel="stylesheet" href="http://f.fontdeck.com/s/css/uYeecfgxZsRY9yoFdZP/14nD1k8/2012.ampersandconf.dev/11761.css" type="text/css" />
+<link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body id="wrapper" class="vevent sponsors">
+
+	<?php include("includes/header.php"); ?>
+
+	<!-- SESSION -->
+	<article class="section session clearfix">
+		
+		<h1>On Web Typography</h1>
+		
+		<h2>Achieving a thorough grasp of typography can take a lifetime, but moving beyond the basics is within your reach right now.</h2>
+		
+		
+		<p>In this talk, We’ll learn how to look at typefaces with a discerning eye, different approaches to typographic planning, how typography impacts the act of reading, and how to choose and combine appropriate typefaces from an aesthetic and technical point of view. Through an understanding of our design tools and how they relate to the web as a medium, we can empower ourselves to use type in meaningful and powerful ways.</p>
+		
+		<aside>
+			<h3>Jason Santa Maria</h3>
+			<p>Jason Santa Maria is the founder and principal of Mighty, a Brooklyn-based design studio. He is creative director of , a faculty member in the MFA Interaction Design program at SVA, vice president of AIGA/NY, co-founder of , founder of , a shared encyclopedia of typefaces online, and creative director for , a magazine for people who make websites.   
+			<nav>
+			
+			</nav>                                
+		</p>
+		</aside>
+		
+	</article>
+	
+		
+	<?php include("includes/footer.php"); ?>
+
+</body>
+</html>
