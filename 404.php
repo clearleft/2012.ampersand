ba52921758478fbe5c3747a0c8df46cbcb0a9bcc
Bevan Stephens <bevan@clearleft.com>
2012-02-24 15:52:34 +0000

    Added 404 page.

---
+++ 404.php
@@ -0,0 +1,28 @@
+<?php include("includes/language-switcher.php");  ?>
+<?php
+$title = "Privacy";
+include("includes/head.php"); 
+?>
+
+<body id="wrapper" class="vevent generic <?php echo $lang; ?>">
+
+<?php include("includes/header.php"); ?>
+
+	<!-- SPONSORS -->
+	<article class="section clearfix">
+		
+		<h1>Missing! Have you seen this page?</h1>
+		
+		<h2>404 &mdash; Page not found.</h2>
+		
+				
+		<p>Sorry but the page you are looking for is not at this address, <br />perhaps you would like to return to the <a href="/">home page</a> and try again.</p>
+		
+			
+	</article>
+	
+		
+<?php include("includes/footer.php"); ?>
+
+</body>
+</html>
