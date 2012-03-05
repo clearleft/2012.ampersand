355c0d39a5595dc73bbaaedef12748242b534a85
Bevan Stephens <bevan@clearleft.com>
2012-03-05 12:31:46 +0000

    Added Yves Peters full info

--- yves-peters.php
+++ yves-peters.php
@@ -12,12 +12,14 @@ include("includes/head.php");
 	<!-- SESSION -->
 	<article class="section session clearfix">
 		
-		<h1 id="sessionTitle" class="no-rule">Detail in Web Typography.</h1>
+		<h1 id="sessionTitle" >Detail in Web Typography.</h1>
+		<h2>For too long web designers had to make do with makeshift solutions for certain typesetting challenges.</h2>
+		<p>Thanks to recent developments in web typography we now have an increased control over the appearance of text on the internet. However this also means we have to get (re)acquainted with the rules in typography. This talk gives an overview of the do's and don't's and offers practical solutions to common problems.</p>
 		
 		<aside class="speaker clearfix">
 			<img src="img/yves-<?php echo $lang; ?>.gif" alt="Yves Peters" width="185" height="196" />
 			<h3 class="yves-peters" href="/yves-peters"><span class="char1">Y</span>ve<span class="swsh">s</span> <span class="char5">P</span>eters</h3>
-			<p>In August 2008 Yves put his graphic design career on the back burner, freeing him to concentrate on his writing and related activities in the digital type business. He writes his Bald Condensed type reviews on <a href="http://typographer.org/">Typographer.org</a> and edits <a href="http://www.fontshop.nl/">Unzipped</a>, the blog on the FontShop BeNeLux home page. Yves also is an accomplished drummer with British/Belgian pop/rock band Troubleman and Americana outfit <a href="http://www.facebook.com/home.php#!/album.php?aid=180255&id=538141732">Rosa Luxe*</a>. His talent for being able to identify most typefaces on sight is utterly useless in daily life.</p>   
+			<p>A little over six years ago graphic designer Yves Peters started reviewing type in his Bald Condensed column on <a href="http://typographer.org">Typographer.org</a>. In August 2008 he put his graphic design career on the back burner, freeing him to concentrate on his writing and related activities in the digital type business. Yves currently edits The FontFeed, a daily dispatch of recommended fonts, typography techniques, and inspirational examples of digital type at work in the real world; and Unzipped, his blog on the FontShop BeNeLux home page. He regularly speaks at schools and international conferences. Yves also is an accomplished drummer with British/American/Belgian pop/rock bands Troubleman and Rosa Luxe*. His talent for being able to identify most typefaces on sight is utterly useless in daily life.</p>   
 			<nav >
 				<ul >
 					<li><a class="website" href="http://fontfeed.com/">Fontfeed.com</a></li>
