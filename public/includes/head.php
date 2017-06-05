992c1c0f079779fd9ba4467cb499df55a7a446d9
Graham Smith <g@blaxian.com>
2017-06-05 12:41:46 +0100

    added missing htaccess file. removed fontdeck. updated deploy server

--- public/includes/head.php
+++ public/includes/head.php
@@ -11,14 +11,13 @@
 <title>Ampersand 2012 &middot; The Web Typography Conference</title>
 <?php } ?>
 <title>Ampersand 2012 &middot; The Web Typography Conference</title>
-<meta name="viewport" content="width=device-width, initial-scale=1"> 
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="Ampersand is an affordable one-day event for knowledgable web designers and typography enthusiasts, held in Brighton on 15 June 2012." />
-<meta name="viewport" content="width=device-width; initial-scale=1; maximum-scale=1" />
 <link rel="shortcut icon" href="/favicon.ico" />
 <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 <!--[if lt IE 9]><script src="js/html5shiv.js"></script><![endif]-->
 <!--[if (gte IE 8)&(lt IE 9)]><script src="js/css3-media-queries.js"></script><![endif]-->
-<link rel="stylesheet" href="http://f.fontdeck.com/s/css/yOkR4W6YJZOSX98WrI1tmpAD3OY/2012.ampersand.dev/11761.css" type="text/css" />
+<!-- <link rel="stylesheet" href="http://f.fontdeck.com/s/css/yOkR4W6YJZOSX98WrI1tmpAD3OY/2012.ampersand.dev/11761.css" type="text/css" /> -->
 <link rel="stylesheet" href="css/styles.css?v=2">
 <?php if($lang=='sw'){ ?>
 	<link rel="stylesheet" href="css/styles-swiss.css">
